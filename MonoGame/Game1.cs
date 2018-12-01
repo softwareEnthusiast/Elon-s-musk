ad6b43b3f161264d00f3984a1df25d8e3173f2dd
ebrukerem <43721976+ebrukerem@users.noreply.github.com>
2018-12-01 17:12:08 +0300

    Create Game1.cs

---
+++ MonoGame/Game1.cs
@@ -0,0 +1,116 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        GameContent gameContent;
+        private Map map;
+        private Puzzle puzzle;
+        private int screenWidth = 0;
+        private int screenHeight = 0;
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            gameContent = new GameContent(Content); screenWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            screenHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            //set game to 502x700 or screen max if smaller
+            if (screenWidth >= 800)
+            {
+                screenWidth = 800;
+            }
+            if (screenHeight >= 700)
+            {
+                screenHeight = 700;
+            }
+            graphics.PreferredBackBufferWidth = screenWidth;
+            graphics.PreferredBackBufferHeight = screenHeight;
+            graphics.ApplyChanges();
+
+            //create game objects
+            int mapX = 0; //we'll center the map on the screen to start
+            int mapY = 0;  //map will be 100 pixels from the bottom of the screen
+            map = new Map(mapX, mapY, screenWidth, spriteBatch, gameContent);  // create the game map
+            int puzzlePieceX = 30;
+            int puzzlePieceY = 30;
+            puzzle = new Puzzle(puzzlePieceX, puzzlePieceY, screenWidth, spriteBatch, gameContent);
+
+
+            // TODO: use this.Content to load your game content here
+        }
+
+    /// <summary>
+    /// UnloadContent will be called once per game and is the place to unload
+    /// game-specific content.
+    /// </summary>
+    protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            map.Draw();
+            puzzle.Draw();
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
