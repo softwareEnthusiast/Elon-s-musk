bfd7a4474ac93d98fc79ac5ea0e98dae2f9985dc
ebrukerem <43721976+ebrukerem@users.noreply.github.com>
2018-12-23 22:27:39 +0300

    Update Form1.Designer.cs

--- IQPuzzlerProSourceCode/Form1.Designer.cs
+++ IQPuzzlerProSourceCode/Form1.Designer.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace IQPuzzleProVer2
 {
     partial class Form2
@@ -28,22 +30,38 @@ namespace IQPuzzleProVer2
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.AllowDrop = true;
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.AutoSize = true;
             this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(0, -3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(903, 607);
+            this.panel1.Size = new System.Drawing.Size(1682, 1061);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
@@ -51,8 +69,37 @@ namespace IQPuzzleProVer2
             this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
             this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1294, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1242, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1190, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
             // button4
             // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button4.AutoSize = true;
             this.button4.BackgroundImage = global::IQPuzzleProVer2.Properties.Resources.getHintButton;
             this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button4.FlatAppearance.BorderSize = 0;
@@ -62,9 +109,12 @@ namespace IQPuzzleProVer2
             this.button4.Size = new System.Drawing.Size(86, 74);
             this.button4.TabIndex = 4;
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.AutoSize = true;
             this.button3.BackgroundImage = global::IQPuzzleProVer2.Properties.Resources.rotateLeftButton;
             this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button3.FlatAppearance.BorderSize = 0;
@@ -77,23 +127,10 @@ namespace IQPuzzleProVer2
             this.button3.Click += new System.EventHandler(this.button3_Click_1);
             this.button3.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             // 
-            // button2
-            // 
-            this.button2.BackgroundImage = global::IQPuzzleProVer2.Properties.Resources.rotateRightButton;
-            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Image = global::IQPuzzleProVer2.Properties.Resources.rotateRightButton;
-            this.button2.Location = new System.Drawing.Point(1219, 208);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(86, 74);
-            this.button2.TabIndex = 2;
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click_1);
-            this.button2.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
-            // 
             // button1
             // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.AutoSize = true;
             this.button1.BackgroundImage = global::IQPuzzleProVer2.Properties.Resources.flipButton;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button1.FlatAppearance.BorderSize = 0;
@@ -108,6 +145,8 @@ namespace IQPuzzleProVer2
             // 
             // button5
             // 
+            this.button5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button5.AutoSize = true;
             this.button5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button5.Location = new System.Drawing.Point(1183, 678);
@@ -117,6 +156,44 @@ namespace IQPuzzleProVer2
             this.button5.Text = "returnMenu";
             this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.BackgroundImage = global::IQPuzzleProVer2.Properties.Resources.rotateRightButton;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = global::IQPuzzleProVer2.Properties.Resources.rotateRightButton;
+            this.button2.Location = new System.Drawing.Point(1219, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 74);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            this.button2.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(1213, 451);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(92, 82);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "Refresh()";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form2
             // 
@@ -130,6 +207,7 @@ namespace IQPuzzleProVer2
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
             this.Name = "Form2";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
@@ -138,8 +216,10 @@ namespace IQPuzzleProVer2
             this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
-            //this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Sprite_MouseMove);
             this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp_1);
+            this.Resize += new System.EventHandler(this.Form2_Resize);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,11 +227,18 @@ namespace IQPuzzleProVer2
 
         #endregion
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         public System.Windows.Forms.Panel panel1;
+        private Button button2;
+        private Timer timer1;
+        private Timer timer2;
+        private Timer timer3;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Button button6;
     }
 }
 
